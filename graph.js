0cd94a1f3ea696adc5f417a4787e98980b3c916d
Jez Ng <jezreel@gmail.com>
2012-07-22 04:38:31 -0700

    save point

--- graph.js
+++ graph.js
@@ -12,24 +12,28 @@ var svg = d3.select("#chart").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+var selectedNode = null;
+
 function redraw(json) {
   force
       .nodes(json.nodes)
       .links(json.links)
       .start();
 
-  var link = svg.selectAll("line.link")
+  var topGroup = svg.append("svg").style("overflow", "visible");
+
+  var link = topGroup.selectAll("line.link")
       .data(json.links)
     .enter().append("line")
       .attr("class", "link")
       .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  var node = svg.selectAll("g.node")
+  var node = topGroup.selectAll("g.node")
       .data(json.nodes)
     .enter().append("g")
       .attr("class", "node")
       .call(force.drag)
-      .on("click", function(d) { rebindAround(d); });
+      .on("click", onClick);
 
   node.append("rect")
       .attr("width", function(d) { return 6 * d.name.length; })
@@ -53,15 +57,10 @@ function redraw(json) {
     node.attr("transform", function(d) { return "translate(" + d.x + ", " + d.y + ")"; });
   });
 
-  function rebindAround(d) {
-    svg.selectAll("g.node").forEach(function(x) { x.fixed = false; });
-    //node.forEach(function(x) { x.fixed = false; });
-    d.fixed = true;
-    d.x = 0;
-    d.y = 0;
-    force.stop();
-    force.charge(-2000).linkDistance(330).start();
+  function onClick(d) {
+    topGroup.attr("x", width/2 - d.x);
+    topGroup.attr("y", height/2 - d.y);
   }
 }
 
-d3.json("data.json", redraw);
+d3.json("font_data_2.json", redraw);
